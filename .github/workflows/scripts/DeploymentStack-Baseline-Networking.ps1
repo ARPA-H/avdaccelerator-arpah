ca1db6c3cda8da6e230275e190f426c0df422a5e
John Ireland <41406288+jwaltireland@users.noreply.github.com>
2025-04-24 09:26:56 -0400

    refactoring

---
+++ .github/workflows/scripts/DeploymentStack-Baseline-Networking.ps1
@@ -0,0 +1,18 @@
+﻿param (
+    [string]$DeploymentStackName,
+    [string]$Location,
+    [string]$deploymentEnvironment,
+    [string]$TemplateFile,
+    [string]$ParametersFile,
+    [string]$avdWorkloadSubsId,
+    [string]$existingHubVnetResourceId,
+    [string]$avdVnetworkAddressPrefixes,
+    [string]$vNetworkAvdSubnetAddressPrefix,
+    [string]$vNetworkPrivateEndpointSubnetAddressPrefix
+)
+
+New-AzSubscriptionDeploymentStack -Name $DeploymentStackName -Location $Location -TemplateFile $TemplateFile -TemplateParameterFile $ParametersFile -P -ActionOnUnmanage "detachAll" -DenySettingsMode "none" `
+    -deploymentEnvironment $deploymentEnvironment `
+    -avdWorkloadSubsId $avdWorkloadSubsId -avdVnetworkAddressPrefixes $avdVnetworkAddressPrefixes `
+    -existingHubVnetResourceId $existingHubVnetResourceId -vNetworkAvdSubnetAddressPrefix vNetworkAvdSubnetAddressPrefix `
+    -vNetworkPrivateEndpointSubnetAddressPrefix $vNetworkPrivateEndpointSubnetAddressPrefix

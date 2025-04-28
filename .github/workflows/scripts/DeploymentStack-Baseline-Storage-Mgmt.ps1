5cc25b0e91a81c5dc62a356f3e286689fe740329
John Ireland <41406288+jwaltireland@users.noreply.github.com>
2025-04-28 16:38:28 -0400

    updates

---
+++ .github/workflows/scripts/DeploymentStack-Baseline-Storage-Mgmt.ps1
@@ -0,0 +1,36 @@
+﻿param (
+    [string]$DeploymentStackName,
+    [string]$Location,
+    [string]$deploymentEnvironment,
+    [string]$TemplateFile,
+    [string]$ParametersFile,
+    [string]$avdWorkloadSubsId,
+    [string]$avdServicePrincipalObjectId,
+    [string]$vdVmLocalUserName,
+    [string]$avdVmLocalUserPassword,
+    [string]$avdSecurityGroups,
+    [string]$update_existing_stack
+)
+
+$parameters = @{
+    Name                                = $DeploymentStackName
+    Location                            = $Location
+    TemplateFile                        = $TemplateFile
+    TemplateParameterFile               = $ParametersFile
+    ActionOnUnmanage                    = "detachAll"
+    DenySettingsMode                    = "none"
+    deploymentEnvironment               = $deploymentEnvironment
+    avdWorkloadSubsId                   = $avdWorkloadSubsId
+    avdServicePrincipalObjectId         = $avdServicePrincipalObjectId
+    vdVmLocalUserName                   = $vdVmLocalUserName
+    avdVmLocalUserPassword              = $avdVmLocalUserPassword
+    avdSecurityGroups                   = $avdSecurityGroups
+}
+
+if ($update_existing_stack -eq 'true') {
+    Write-Host "Updating existing stack"
+    Set-AzSubscriptionDeploymentStack @parameters
+} else {
+    Write-Host "Creating new stack"
+    New-AzSubscriptionDeploymentStack @parameters
+}

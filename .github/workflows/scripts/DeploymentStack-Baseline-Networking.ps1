1a8ba2f6141bf084661d327f5d7db0167ff7983f
John Ireland <41406288+jwaltireland@users.noreply.github.com>
2025-04-28 08:29:32 -0400

    -vNetworkAvdSubnetAddressPrefix param was missing $

--- .github/workflows/scripts/DeploymentStack-Baseline-Networking.ps1
+++ .github/workflows/scripts/DeploymentStack-Baseline-Networking.ps1
@@ -12,20 +12,43 @@
     [string]$update_existing_stack
 )
 
+# if ($update_existing_stack -eq 'true') {
+#     Write-Host "Updating existing stack"
+#     Set-AzSubscriptionDeploymentStack -Name $DeploymentStackName -Location $Location -TemplateFile $TemplateFile -TemplateParameterFile $ParametersFile -P -ActionOnUnmanage "detachAll" -DenySettingsMode "none" `
+#         -deploymentEnvironment $deploymentEnvironment `
+#         -avdWorkloadSubsId $avdWorkloadSubsId -avdVnetworkAddressPrefixes $avdVnetworkAddressPrefixes `
+#         -existingHubVnetResourceId $existingHubVnetResourceId -vNetworkAvdSubnetAddressPrefix $vNetworkAvdSubnetAddressPrefix `
+#         -vNetworkPrivateEndpointSubnetAddressPrefix $vNetworkPrivateEndpointSubnetAddressPrefix
+#     return
+# } else {
+#     Write-Host "Creating new stack"
+#     New-AzSubscriptionDeploymentStack -Name $DeploymentStackName -Location $Location -TemplateFile $TemplateFile -TemplateParameterFile $ParametersFile -P -ActionOnUnmanage "detachAll" -DenySettingsMode "none" `
+#     -deploymentEnvironment $deploymentEnvironment `
+#     -avdWorkloadSubsId $avdWorkloadSubsId -avdVnetworkAddressPrefixes $avdVnetworkAddressPrefixes `
+#     -existingHubVnetResourceId $existingHubVnetResourceId -vNetworkAvdSubnetAddressPrefix $vNetworkAvdSubnetAddressPrefix `
+#     -vNetworkPrivateEndpointSubnetAddressPrefix $vNetworkPrivateEndpointSubnetAddressPrefix
+#     return
+# }
+$parameters = @{
+    Name                                = $DeploymentStackName
+    Location                            = $Location
+    TemplateFile                        = $TemplateFile
+    TemplateParameterFile               = $ParametersFile
+    # P                                   = $true
+    ActionOnUnmanage                    = "detachAll"
+    DenySettingsMode                    = "none"
+    deploymentEnvironment               = $deploymentEnvironment
+    avdWorkloadSubsId                   = $avdWorkloadSubsId
+    avdVnetworkAddressPrefixes          = $avdVnetworkAddressPrefixes
+    existingHubVnetResourceId           = $existingHubVnetResourceId
+    vNetworkAvdSubnetAddressPrefix      = $vNetworkAvdSubnetAddressPrefix
+    vNetworkPrivateEndpointSubnetAddressPrefix = $vNetworkPrivateEndpointSubnetAddressPrefix
+}
+
 if ($update_existing_stack -eq 'true') {
     Write-Host "Updating existing stack"
-    Set-AzSubscriptionDeploymentStack -Name $DeploymentStackName -Location $Location -TemplateFile $TemplateFile -TemplateParameterFile $ParametersFile -P -ActionOnUnmanage "detachAll" -DenySettingsMode "none" `
-        -deploymentEnvironment $deploymentEnvironment `
-        -avdWorkloadSubsId $avdWorkloadSubsId -avdVnetworkAddressPrefixes $avdVnetworkAddressPrefixes `
-        -existingHubVnetResourceId $existingHubVnetResourceId -vNetworkAvdSubnetAddressPrefix $vNetworkAvdSubnetAddressPrefix `
-        -vNetworkPrivateEndpointSubnetAddressPrefix $vNetworkPrivateEndpointSubnetAddressPrefix
-    return
+    Set-AzSubscriptionDeploymentStack @parameters
 } else {
     Write-Host "Creating new stack"
-    New-AzSubscriptionDeploymentStack -Name $DeploymentStackName -Location $Location -TemplateFile $TemplateFile -TemplateParameterFile $ParametersFile -P -ActionOnUnmanage "detachAll" -DenySettingsMode "none" `
-    -deploymentEnvironment $deploymentEnvironment `
-    -avdWorkloadSubsId $avdWorkloadSubsId -avdVnetworkAddressPrefixes $avdVnetworkAddressPrefixes `
-    -existingHubVnetResourceId $existingHubVnetResourceId -vNetworkAvdSubnetAddressPrefix $vNetworkAvdSubnetAddressPrefix `
-    -vNetworkPrivateEndpointSubnetAddressPrefix $vNetworkPrivateEndpointSubnetAddressPrefix
-    return
+    New-AzSubscriptionDeploymentStack @parameters
 }
